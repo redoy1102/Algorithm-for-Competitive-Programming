3ea9b5887158578bc7f65846580670947b29d80c
Fuad <20103046@iubat.edu>
2023-09-11 11:01:57 +0600

    Problem of prime number algo is fixed.

--- prime_number.py
+++ prime_number.py
@@ -3,10 +3,11 @@ import math as ma       # imported math for the sqrt()
 def prime_number(n):
     if n <= 1:
         return n
-    for i in range(2, int(ma.sqrt(n)), n):
+    for i in range(2, int(ma.sqrt(n)) + 1):
         if n % i == 0:
             return False
 
     return True
 
+print(prime_number(4))
 
